30865827bf007312330da451f83fc0bf50b7960f
frost <113864220+frostboyz@users.noreply.github.com>
2022-09-20 21:42:37 -0300

    Add files via upload

---
+++ src/js/links.js
@@ -0,0 +1,10 @@
+const steam = () => (window.open("https://steamcommunity.com/id/satanking"));
+const valorant = () => (window.open("https://dak.gg/valorant/profile/revoke/ilyfirst-vktag"));
+const instagram = () => (window.open("https://instagram.com/1337vc"));
+const twitter = () => (window.open("https://twitter.com/ceonubank"));
+const spotify = () => (window.open("https://open.spotify.com/user/fjctlp29jy8e3gulm8bo928hy"));
+const pinterest = () => (window.open("https://br.pinterest.com/devilgohies/"));
+
+// OTHERS
+
+const arch = () => (window.open("https://discord.gg/CzSeAt9D"));
